b10ace063ff3e1fcce247670ffcb7f35ef69c7f1
Zouhair Fouiguira <zouhairfgra@gmail.com>
2023-10-22 13:00:00 +0100

    update app.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './index.css';
 import App from './App';
+import { ContextProvider } from './contexts/ContextProvider';
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+ReactDOM.render(
+  <React.StrictMode>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
